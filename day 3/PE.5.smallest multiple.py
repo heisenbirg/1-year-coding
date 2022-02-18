4e63b452c256f55b54ae05a0a2014bbc4ae619c7
heisenbirg <92167560+heisenbirg@users.noreply.github.com>
2022-02-18 18:42:29 +0530

    Add files via upload

---
+++ day 3/PE.5.smallest multiple.py
@@ -0,0 +1,3 @@
+import math
+n=20
+print(math.lcm(*list(range(1,n+1))))
---
+++ day 3/PE.6.sum square difference.py
@@ -0,0 +1,11 @@
+import math
+n=100
+print((n*(3*(n**3) + 2*(n**2) - 3*n - 2))//12)
+''' the above formula is derived from sum of n natural numbers formula
+    1+2+3....n=(n * (n + 1) )/2
+
+    and sum of squares of n natural numbers formula
+
+    1**2 + 2**2 +3**2 ... n**2= n* ( n+1 ) * ( 2 * n + 1 )/6
+
+    '''
