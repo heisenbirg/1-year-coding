625d1740affb4a2eb6e4e207ce3019871709871a
heisenbirg <92167560+heisenbirg@users.noreply.github.com>
2022-02-19 19:40:33 +0530

    Add files via upload

---
+++ day 4/PE.7.10001st prime.py
@@ -0,0 +1,21 @@
+import itertools,math
+def is_prime(x):
+	if x <= 1:
+		return False
+	elif x <= 3:
+		return True
+	elif x % 2 == 0:
+		return False
+	else:
+		for i in range(3, int(pow(x,0.5)) + 1, 2):
+			if x % i == 0:
+				return False
+		return True
+
+def compute():
+	ans = next(itertools.islice(filter(is_prime, itertools.count(2)), 10000, None))
+	return str(ans)
+
+
+if __name__ == "__main__":
+	print(compute())
